316ca944ab24b9e2a47fc85aea7f4e15b4b208a7
Steve Ives <stevei@synergex.com>
2020-03-30 17:50:56 -0700

    Finished updating for .NET Core 3.1

--- HarmonyCoreDemoIIS_PUBLISH.bat
+++ HarmonyCoreDemoIIS_PUBLISH.bat
@@ -18,5 +18,12 @@ copy TraditionalBridge\bin\Debug\x64\launch.bat %DeployDir%
 copy TraditionalBridge\bin\Debug\x64\TraditionalBridgeHost.dbr %DeployDir%
 
 popd
+
+rem ----------------------------------------------------------------------------
+rem This appears to be necessary for now as Azure AppService does not appear to 
+rem currently support the VS2019 C++ runtimes!!!
+copy vcredistFiles\*.* %DeployDir%
+rem ----------------------------------------------------------------------------
+
 endlocal
 pause
