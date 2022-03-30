c1e8223cd01bb5eae43fe5b61cdb51bc1625fd2c
Steve Ives <steve@steveives.com>
2022-03-30 11:18:02 -0700

    Publish web.config

--- HarmonyCoreDemoIIS_PUBLISH.bat
+++ HarmonyCoreDemoIIS_PUBLISH.bat
@@ -17,7 +17,12 @@ copy SampleData\*.* %DeployDir%\SampleData
 copy EXE\launch.bat %DeployDir%
 copy EXE\TraditionalBridgeHost.dbr %DeployDir%
 
+if exist %DeployDir%\web.config del /q %DeployDir%\web.config
+copy web.config %DeployDir%\web.config
+
 popd
+
+
 
 rem ----------------------------------------------------------------------------
 rem This appears to be necessary for now as Azure AppService does not appear to 
