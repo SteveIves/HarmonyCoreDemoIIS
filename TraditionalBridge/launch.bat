b1561af0cce32139403e31b4293e9b7c21c089b4
Steve Ives <steve.ives@synergex.com>
2022-07-14 18:55:32 -0700

    Modify launch.bat to set logging level based on P1

--- TraditionalBridge/launch.bat
+++ TraditionalBridge/launch.bat
@@ -1,12 +1,22 @@
 @echo off
 pushd %~dp0
-setlocal
-
-rem Set any environment required by the underlying methods
+setlocal EnableDelayedExpansion
 
 rem Configure a Synergy environment
 rem call "%SYNERGYDE32%dbl\dblvars32.bat"
 call "%SYNERGYDE64%dbl\dblvars64.bat"
+
+rem Set any environment required by the underlying code
+
+
+
+
+rem If we have a log level, use it, else default to 2
+if "%~1"=="" (
+  set HARMONY_LOG_LEVEL=2
+) else (
+  set HARMONY_LOG_LEVEL=%1
+)
 
 rem Launch the host program
 dbs host.dbr
