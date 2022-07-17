e38efb51f5acf50f6afa19245964c384c69144f9
Steve Ives <steve.ives@synergex.com>
2022-07-16 18:14:10 -0700

    More publish and deploy improvements

--- publish.bat
+++ publish.bat
@@ -37,10 +37,11 @@ set SolutionDir=%~dp0
 
 pushd "%SolutionDir%"
 
+rem If we have a settings file, execute it
 if exist publish_settings.bat call publish_settings.bat
 
-set rpsmfil=%SolutionDir%Repository\rpsmain.ism
-set rpstfil=%SolutionDir%Repository\rpstext.ism
+set RPSMFIL=%SolutionDir%Repository\rpsmain.ism
+set RPSTFIL=%SolutionDir%Repository\rpstext.ism
 
 set DeployDir=%SolutionDir%PUBLISH
 
@@ -51,13 +52,13 @@ if exist "%DeployDir%\." (
 )
 
 rem Publish the application
-echo INFO: Publishing the application to the deployment folder...
+echo INFO: Publishing to %DeployDir%...
 set SYNCPMOPT=-wd=316
 pushd Services.Host
-dotnet publish -c Debug -r win7-x64 --self-contained -o %DeployDir%
+dotnet publish -nologo --configuration Debug --runtime win10-x64 --self-contained --output %DeployDir% --verbosity quiet
 
 if ERRORLEVEL 0 (
-  echo INFO: Publish is complete
+  echo INFO: Publish complete
 ) else (
   echo ERROR: Publish failed!
   popd
@@ -67,15 +68,15 @@ if ERRORLEVEL 0 (
 popd
 
 rem Include the Traditional Bridge host program and startup script
-echo INFO: Providing Traditional Bridge files...
-copy /y TraditionalBridge\EXE\host.dbr %DeployDir%
-copy /y TraditionalBridge\EXE\host.dbp %DeployDir%
-copy /y TraditionalBridge\EXE\launch.bat %DeployDir%
+echo INFO: Copying traditional bridge files...
+copy /y TraditionalBridge\EXE\host.dbr %DeployDir% > nul 2>&1
+copy /y TraditionalBridge\EXE\host.dbp %DeployDir% > nul 2>&1
+copy /y TraditionalBridge\EXE\launch.bat %DeployDir% > nul 2>&1
 
 rem Replace the web.config file with our own version that sets the
 rem ASPNETCORE_ENVIRONMENT to Production
 if exist "%DeployDir%\web.config" (
-  echo INFO: Providing custom web.config file...
+  echo INFO: Copying web.config...
   del  /Q "%DeployDir%\web.config"
   copy /Y "%SolutionDir%\web.config" "%DeployDir%\web.config" > nul 2>&1
 )
@@ -83,16 +84,16 @@ if exist "%DeployDir%\web.config" (
 rem If you want to include the SampleData folder set INCLUDE_SAMPLE_DATA=YES
 rem in PUBLISH_SETTINGS.BAT
 if defined INCLUDE_SAMPLE_DATA (
-  echo INFO: Providing sample data...
+  echo INFO: Copying sample data...
   if not exist %DeployDir%\SampleData\. mkdir %DeployDir%\SampleData
-  copy /y SampleData\*.* %DeployDir%\SampleData
+  copy /y SampleData\*.* %DeployDir%\SampleData > nul 2>&1
 )
 
 rem At the time of writing, Azure AppService does not provide the VS2019 C++
-rem runtimes so if we are publishing for AppService we need to include them
+rem runtime so if we are publishing for AppService we need to include it
 if defined INCLUDE_C_RUNTIME (
-  echo INFO: Providing C++ runtimes...
-  copy /y vcredistFiles\*.* %DeployDir%
+  echo INFO: Copying C++ runtime...
+  copy /y vcredistFiles\*.* %DeployDir% > nul 2>&1
 )
 
 rem If WinZip is present, Zip the PUBLISH folder to a date-stamped zip file
@@ -103,12 +104,12 @@ set zipFile=%SolutionDir%HarmonyCoreService-%yyyymmdd%-%hh%%mm%.zip
 
 if exist %DeployDir%\. (
   if exist "%ProgramW6432%\7-Zip\7z.exe" (
-    echo INFO: Zipping the published application to %zipFile%
+    echo INFO: Zipping to %zipFile%...
     pushd %DeployDir%
-    "%ProgramW6432%\7-Zip\7z.exe" a -r "%zipFile%" *
+    "%ProgramW6432%\7-Zip\7z.exe" a -r -bso0 -bsp0 "%zipFile%" *
 
     if ERRORLEVEL 0 (
-      echo INFO: The zip file was created successfully
+      echo INFO: Zip file created
     ) else (
       echo ERROR: Failed to create zip file!
       popd
@@ -121,46 +122,87 @@ if exist %DeployDir%\. (
   )
 )
 
-rem Does the zip file exist?
+rem If the zip file exists, delete the publish folder
 if exist "%zipFile%" (
-
-  rem Yes, delete the PUBLISH folder
+  echo INFO: Deleting deployment folder %DeployDir%
   rmdir /S /Q "%DeployDir%" > nul 2>&1
-
-    rem Do we have an FTP username?
-    if not defined PUBLISH_FTP_SERVER (
-        echo WARNING: Unable to upload the zip file to staging server because PUBLISH_FTP_SERVER is not set!
-        goto done
-    )
-    rem Do we have an FTP username?
-    if not defined PUBLISH_FTP_USER (
-        echo WARNING: Unable to upload the zip file to staging server because PUBLISH_FTP_USER is not set!
-        goto done
-    )
-
-    rem Do we have an FTP password?
-    if not defined PUBLISH_FTP_PASSWORD (
-        echo WARNING: Unable to upload the zip file to staging server because PUBLISH_FTP_USER is not set!
-        goto done
-    )
-
-    rem Is WinSCP installed?
-    if exist "%ProgramFiles(x86)%\WinSCP\WinSCP.com" (
-
-        echo INFO: Uploading zip file to staging server...
-
-        rem Yes, upload the ZIP file to the staging server
-        "%ProgramFiles(x86)%\WinSCP\WinSCP.com" /command "open ftp://%PUBLISH_FTP_USER%:%PUBLISH_FTP_PASSWORD%@%PUBLISH_FTP_SERVER%/" "put %zipFile% /" "exit"
-
-        if ERRORLEVEL 0 (
-        echo SUCCESS: The zip file was successfully uploaded to the staging server.
-        ) else (
-        echo ERROR: Failed to upload the zip file to the staging server!
-        )
-    ) else (
-        echo WARNING: Unable to upload the zip file to staging server because WinSCP is not installed!
-    )
+) else (
+    goto done
 )
+
+rem Do we have PUBLISH_MODE set?
+if not defined PUBLISH_MODE (
+  echo INFO: Zip file distribution has not been configured
+  goto done
+)
+
+rem Distribution via COPY?
+if "%PUBLISH_MODE%" == "COPY" (
+  echo COPY MODE!
+
+  rem Do we have a direrctory to copy to?
+  if not defined PUBLISH_DIR (
+    echo ERROR: Unable to copy zip file to staging server because PUBLISH_DIR is not set!
+    goto done
+  )
+
+  rem Does the direrctory exist?
+  if not exist "%PUBLISH_DIR%\." (
+    echo ERROR: Unable to copy zip file to staging server because directory %PUBLISH_DIR% does not exist!
+    goto done
+  )
+
+  rem Copy the file
+  copy /y "%zipFile%" "%PUBLISH_DIR%" > nul 2>&1
+
+  rem Did it work?
+  if ERRORLEVEL 0 (
+    echo INFO: The zip file was successfully copied to "%PUBLISH_DIR%"
+  ) else (
+    echo ERROR: Failed to copy zip file to "%PUBLISH_DIR%"
+  )
+
+  goto done
+)
+
+rem Distribution via FTP?
+if "%PUBLISH_MODE%" == "FTP" (
+
+  rem Do we have an FTP username?
+  if not defined PUBLISH_FTP_SERVER (
+    echo WARNING: Unable to FTP the zip file to staging server because PUBLISH_FTP_SERVER is not set!
+    goto done
+  )
+
+  rem Do we have an FTP username?
+  if not defined PUBLISH_FTP_USER (
+    echo WARNING: Unable to FTP the zip file to staging server because PUBLISH_FTP_USER is not set!
+    goto done
+  )
+
+  rem Do we have an FTP password?
+  if not defined PUBLISH_FTP_PASSWORD (
+    echo WARNING: Unable to FTP the zip file to staging server because PUBLISH_FTP_USER is not set!
+    goto done
+  )
+
+  rem Is WinSCP installed?
+  if exist "%ProgramFiles(x86)%\WinSCP\WinSCP.com" (
+    echo INFO: Uploading zip file to staging server...
+    rem Yes, upload the ZIP file to the staging server
+    "%ProgramFiles(x86)%\WinSCP\WinSCP.com" /command "open ftp://%PUBLISH_FTP_USER%:%PUBLISH_FTP_PASSWORD%@%PUBLISH_FTP_SERVER%/" "put %zipFile% /" "exit"
+     if ERRORLEVEL 0 (
+       echo SUCCESS: The zip file was successfully uploaded to the staging server.
+     ) else (
+       echo ERROR: Failed to upload the zip file to the staging server!
+     )
+  ) else (
+      echo WARNING: Unable to upload the zip file to staging server because WinSCP is not installed!
+  )
+  goto done
+)
+
+echo INFO: PUBLISH_MODE is set to an unsupported value %PUBLISH_MODE%. Use COPY or FTP.
 
 :done
 popd
